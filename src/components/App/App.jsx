850c4b5f15cb8c33de19dd687c69a6247d87571b
Ld-monkey <lfourteau91@gmail.com>
2024-04-30 00:13:57 +0200

    change to simple routes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,11 @@
 import { Route, Switch } from 'wouter';
-import { useHashLocation } from 'wouter/use-hash-location';
 import SubscribePage from '../../pages/Subscribe/SubscribePage';
 import SuccessPage from '../../pages/Success/SuccessPage';
 
 function App() {
   return (
     <Switch>
-      <Route hook={useHashLocation}>
+      <Route>
         <Route path={import.meta.env.BASE_URL} component={SubscribePage} />
         <Route path="/success" component={SuccessPage} />
       </Route>
