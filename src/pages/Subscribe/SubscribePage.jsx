4532c1c1815b62cd7ea5aa076df4495c5f17fbf2
Ld-monkey <lfourteau91@gmail.com>
2024-04-30 23:38:17 +0200

    design page for desktop interface

--- src/pages/Subscribe/SubscribePage.jsx
+++ src/pages/Subscribe/SubscribePage.jsx
@@ -30,13 +30,11 @@ function SubscribePage() {
 
   function handleButtonValidation() {
     if (validateEmail(email)) {
-      console.log('valide email yes');
       setValideEmail(true);
       setShowError(false);
 
       navigate('/success', { state: { email: email } });
     } else {
-      console.log('no valide email');
       setValideEmail(false);
       setShowError(true);
     }
@@ -48,58 +46,62 @@ function SubscribePage() {
   }
 
   return (
-    <main>
-      <section className="flex flex-col">
-        <div className="min-h-[284px] bg-signup_mobile bg-cover bg-center sm:bg-signup_desktop" />
-        <article className="flex flex-col gap-10 px-6 py-10">
-          <div className="flex flex-col gap-6">
-            <h1 className="text-4xl font-bold text-dark-slate-grey">
+    <main className="flex h-screen items-center justify-center bg-dark-slate-grey">
+      <section className="mx-9 flex w-[928px] flex-col bg-white sm:h-[641px] sm:flex-row-reverse sm:justify-between sm:gap-6 sm:rounded-[36px] sm:p-6">
+        <div className="min-h-[284px] w-[400px] bg-signup_mobile bg-cover bg-center bg-no-repeat sm:rounded-2xl sm:bg-slate-900 sm:bg-signup_desktop" />
+        <article className="flex flex-1 flex-col gap-10 px-6 py-10 sm:items-center sm:justify-center sm:px-10">
+          <div className="flex w-[376px] flex-col gap-6">
+            <h1 className="text-4xl font-bold text-dark-slate-grey sm:text-[56px]">
               Stay updated!
             </h1>
-            <p>Join 60,000+ product managers receiving monthly updates on:</p>
-            <ul className="ml-6 space-y-2.5">
-              <li className="list-image-icon-success pl-[11px] ">
+            <p className="text-dark-slate-grey">
+              Join 60,000+ product managers receiving monthly updates on:
+            </p>
+            <ul className="ml-6 space-y-2.5 text-dark-slate-grey">
+              <li className="list-image-icon-success pl-[11px]">
                 <p className="relative -top-[5.5px]">
                   Product discovery and building what matters
                 </p>
               </li>
-              <li className="pl-[11px]">
+              <li className="list-image-icon-success pl-[11px]">
                 <p className="relative -top-[5.5px]">
                   Measuring to ensure updates are a success
                 </p>
               </li>
-              <li className="pl-[11px]">
+              <li className="list-image-icon-success pl-[11px]">
                 <p className="relative -top-[5px]">And much more!</p>
               </li>
             </ul>
           </div>
-          <form onSubmit={(e) => handleSubmitForm(e)}>
-            <div className="flex justify-between">
-              <label
-                htmlFor="email"
-                className="text-xs font-bold text-dark-slate-grey"
-              >
-                Email address
-              </label>
-              {showError && (
-                <span className="text-xs font-bold text-tomato">
-                  Valid email required
-                </span>
-              )}
-            </div>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className={`mb-6 mt-2 w-full rounded-lg border border-grey px-6 py-4  ${showError ? 'border-tomato bg-tomato/15 text-tomato' : ''}`}
-              placeholder="email@company.com"
-              onChange={(e) => handleEmail(e.target.value)}
-              required
-            />
-            <Button setNavigation={() => handleButtonValidation}>
-              Subscribe to monthly newsletter
-            </Button>
-          </form>
+          <div className="w-[376px]">
+            <form onSubmit={(e) => handleSubmitForm(e)}>
+              <div className="flex justify-between">
+                <label
+                  htmlFor="email"
+                  className="text-xs font-bold text-dark-slate-grey"
+                >
+                  Email address
+                </label>
+                {showError && (
+                  <span className="text-xs font-bold text-tomato">
+                    Valid email required
+                  </span>
+                )}
+              </div>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className={`mb-6 mt-2 w-full rounded-lg border border-grey px-6 py-4  ${showError ? 'border-tomato bg-tomato/15 text-tomato' : ''} text-dark-slate-grey`}
+                placeholder="email@company.com"
+                onChange={(e) => handleEmail(e.target.value)}
+                required
+              />
+              <Button setNavigation={() => handleButtonValidation}>
+                Subscribe to monthly newsletter
+              </Button>
+            </form>
+          </div>
         </article>
       </section>
     </main>
